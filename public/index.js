e754bfbc9af9aed513c5e8efcb90903fa0373e30
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2022-10-31 20:34:56 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,17 +1,11 @@
 $("#submit").click(()=>{
     let input = $("#input").val();
-
-    if(input === ''){
-      let today = new Date();
-      input =  `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
-    }
     
-      fetch("http://localhost:3000/api/"+input)
+      fetch(`https://Timestamp-Microservice.alfiyasiddique.repl.co/api/${input}`)
          .then(res=>{
             return res.json();
          })
          .then(data=>{
-            data = JSON.parse(data);
             if(data.hasOwnProperty("error")){
             $("#result").html(`<h3>Invalid Date OR Invalid Date Format</h3>`)
 
